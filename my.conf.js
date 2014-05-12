dcaeeabe94d064dd9c7caa761f0bf0437a0c5f4b
Roy Riojas <roy@kno.com>
2014-05-11 18:59:13 -0700

    Update karma-qunit to work with the latest qunit

--- my.conf.js
+++ my.conf.js
@@ -2,7 +2,7 @@ module.exports = function(config) {
   config.set({
     basePath: '',
     autoWatch: true,
-    frameworks: ['qunit'],
+    frameworks: ['qunit', 'qunit-sb'],
     files: [
       '*.js',
       'test/*.js'
--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,15 @@
+singleModule('nothing more here', {
+
+});
+
 test("sqrt", function() {
 
   deepEqual(My.sqrt(4), 2, "square root of 4 is 2");
 
+});
+
+module('my module');
+
+test('testing equality', function () {
+  equal(1, 2, 'Not true');
 });
